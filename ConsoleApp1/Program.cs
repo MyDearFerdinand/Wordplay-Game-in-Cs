563151f5609ed91d01c16f1104937267398636e6
Vladyslav Maidaniuk <vlad.maidanyuk95@gmail.com>
2023-06-06 15:16:40 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> words = new List<string>() { "apple", "banana", "cherry", "watermelon", "orange" };
+
+        Random rand = new Random();
+        string chosenWord = words[rand.Next(0, words.Count)];
+
+        Console.WriteLine("Welcome to Word Guess Game!");
+        Console.WriteLine("I have chosen a fruit name. It has " + chosenWord.Length + " letters. Can you guess it?");
+
+        string guessedWord = new string('_', chosenWord.Length);
+        while (guessedWord.Contains('_'))
+        {
+            Console.WriteLine(guessedWord);
+            Console.WriteLine("Enter a letter:");
+            char letter = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < chosenWord.Length; i++)
+            {
+                if (chosenWord[i] == letter)
+                {
+                    guessedWord = guessedWord.Remove(i, 1);
+                    guessedWord = guessedWord.Insert(i, letter.ToString());
+                }
+            }
+
+            if (!chosenWord.Contains(letter))
+            {
+                Console.WriteLine("No, the word doesn't contain the letter " + letter);
+            }
+        }
+
+        Console.WriteLine(guessedWord);
+        Console.WriteLine("Congratulations! You've guessed the word!");
+    }
+}
